b77c8f4bb5332883cd16a1ff263576e43a0e39af
Key Yu Wan <keyflcbyuwan@gmail.com>
2022-06-21 21:38:45 -0300

    Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,9 @@
-import { gql, useQuery } from "@apollo/client";
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`;
+import { Event } from "./pages/Event";
 
 export function App() {
-  const { data } = useQuery(GET_LESSONS_QUERY);
-  console.log(data);
   return (
     <div>
-      <h1>Ignite lab</h1>
+      <Event />
     </div>
   );
 }
