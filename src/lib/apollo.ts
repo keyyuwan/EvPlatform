984d37e34f321969181f2673f1a47e02f6e4fc07
Key Yu Wan <keyflcbyuwan@gmail.com>
2022-06-23 22:43:10 -0300

    create subscriber mutation

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "https://api-sa-east-1.graphcms.com/v2/cl4o71rz810il01z2d2e567s9/master",
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`,
+  },
   cache: new InMemoryCache(),
 });
