7101020d29182e356fe477ae442b1522f47404b0
Key Yu Wan <keyflcbyuwan@gmail.com>
2022-06-25 00:54:30 -0300

    Refactor: Subscribe page responsive

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -28,24 +28,24 @@ export function Subscribe() {
 
   return (
     <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-      <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
-        <div className="max-w-[640px]">
+      <div className="w-full max-w-[1100px] flex flex-col mt-10 mx-auto lg:mt-20 lg:flex-row lg:items-center lg:justify-between">
+        <div className="max-w-[640px] flex flex-col items-center text-center px-6 lg:block lg:text-left lg:py-0">
           <Logo />
 
-          <h1 className="mt-8 text-[2.5rem] leading-tight">
+          <h1 className="mt-8 text-[1.875rem] leading-tight lg:text-[2.5rem]">
             Construa uma{" "}
             <strong className="text-blue-500">aplicação completa</strong>, do
             zero, com <strong className="text-blue-500">React JS</strong>
           </h1>
-          <p className="mt-4 text-gray-200 leading-relaxed">
+          <p className="text-sm mt-6 text-gray-200 leading-relaxed lg:text-base lg:mt-4">
             Em apenas uma semana você vai dominar na prática uma das tecnologias
             mais utilizadas e com alta demanda para acessar as melhores
             oportunidades do mercado.
           </p>
         </div>
 
-        <div className="p-8 bg-gray-700 border border-gray-500 rounded">
-          <strong className="text-2xl mb-6 block">
+        <div className="mt-8 p-8 bg-gray-700 border border-gray-500 lg:rounded lg:mt-0">
+          <strong className="text-lg mb-6 block lg:text-2xl">
             Inscreva-se gratuitamente
           </strong>
 
